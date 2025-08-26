13fe8fa53f0da8e8b681d01f8405095917c88838
rashtraIQ <rashtra.innovquant@gmail.com>
2025-08-26 16:36:55 +0530

    this is example file

---
+++ test/testFile.swift
@@ -0,0 +1,18 @@
+//
+//  testFile.swift
+//  mE Health
+//
+//  Created by Rashtra Humane on 26/08/25.
+//
+
+import SwiftUI
+
+struct testFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testFile()
+}
